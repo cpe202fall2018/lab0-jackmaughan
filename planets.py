3370ce461d9bed303c2df382a7f32200d7434a4c
Jack Maughan <jmaughan@calpoly.edu>
2018-09-22 23:39:12 -0700

    with comments el fin

--- planets.py
+++ planets.py
@@ -1,3 +1,13 @@
+#
+#Name:  Jack Maughan
+#Student ID: 012288610
+#Date (last modified): 9/22/2018
+#
+# Lab 0
+# Section 13/14
+# Purpose of Lab: Introduce Python
+# additional comments
+
 def weight_on_planets():
 	weight = int(input("What do you weigh on earth? "))
 	mars = weight * .38
